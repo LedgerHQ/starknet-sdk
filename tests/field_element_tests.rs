b0e790a311e1527036778f456c8fb31db14e2c3f
Thomas Marchand <thomas.marchand@tuta.io>
2023-06-19 14:36:12 +0200

    feat: add tests for division operations

--- tests/field_element_tests.rs
+++ tests/field_element_tests.rs
@@ -5,7 +5,7 @@ fn test_addition() {
     let fe1 = FieldElement::ONE;
     let fe2 = FieldElement::ONE;
     let fe3 = &fe1 + &fe2;
-    
+
     assert_eq!(fe3, FieldElement::TWO);
 }
 
@@ -37,13 +37,32 @@ fn test_modulo() {
 }
 
 #[test]
+fn test_div_rem() {
+    let fe1 = FieldElement::from(10);
+    let fe2 = FieldElement::from(3);
+    let (quotient, remainder) = fe1.div_rem(&fe2);
+
+    assert_eq!(quotient, FieldElement::from(3));
+    assert_eq!(remainder, FieldElement::from(1));
+}
+
+#[test]
+fn test_field_division() {
+    let fe1 = FieldElement::from(6);
+    let fe2 = FieldElement::from(3);
+    let fe3 = &fe1 / &fe2;
+
+    assert_eq!(fe3, FieldElement::from(2));
+}
+
+#[test]
 fn test_chain_operations() {
     let fe1 = FieldElement::TWO;
     let fe2 = FieldElement::ONE;
     let fe3 = &fe1 + &fe2; // result should be 3
     let fe4 = &fe3 * &fe1; // result should be 6
     let fe5 = &fe4 - &fe3; // result should be 3
-    let fe6 = &fe5 % &fe2; // result should be 0
+    let fe6 = &fe5 % &fe1; // result should be 1
 
-    assert_eq!(fe6, FieldElement::ZERO);
+    assert_eq!(fe6, FieldElement::ONE);
 }
